379a713a40f9d273f29cc0ae4ff177aa885fabc5
elsamcgu <elsamcguinn18@gmail.com>
2022-11-27 14:33:48 +0000

    new

---
+++ demo.js
@@ -0,0 +1,33 @@
+// for(var i=0; i<6; i++){
+// }
+// console.log("hello")
+
+
+
+// var person= {
+//     name: 'Jenny',
+//     age: '23',
+    
+// }
+
+// console.log(person.name)
+
+// var book= {
+//     characters: ['edward', 'Bella', 'jacob'],
+//     location: "forks",
+//     }
+    
+//     console.log(book.characters[1])
+
+// const person = {
+//     firstName: 'Elizabeth',
+//     lastName: 'Harmon',
+//     age: 22,
+//     eyeColor: 'Hazel',
+//     greeting: function () {
+//       return `Hi, I am ${this.firstName} ${this.lastName}.`;
+//     },
+//   };
+  
+//   console.log(person.greeting());
+
